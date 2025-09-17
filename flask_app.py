1ea7a21f5d411269ce773e300ad12f2770c105c9
Aviral Paliwal <aviral.paliwal007@gmail.com>
2025-09-17 14:49:45 +0530

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,8 @@
+#This code is a sample Flask application
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def HelloWorld():
+    return 'Hello, World!'
+if __name__ == '__main__':
+    app.run(debug=True)
